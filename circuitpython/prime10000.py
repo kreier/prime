d718be7c9da2bb68a963002b20ecd05abaff1124
SSIS students <43933271+kreier@users.noreply.github.com>
2022-10-21 12:41:58 +0700

    faster algorithms

--- circuitpython/prime10000.py
+++ circuitpython/prime10000.py
@@ -1,23 +1,19 @@
 import math, time
-
 last = 10000
-count = 4          # we start from 11, know 2, 3, 5, 7
-start = time.monotonic()
+found = 4          # we start from 11, know 2, 3, 5, 7
 print(f"Prime numbers to {last}")
-
 #print('2, 3, 5, 7',end='')
+start = time.monotonic()
 for number in range(11, last, 2):
     prime = True
     for divider in range(3, int(math.sqrt(number))+1, 2):
-        if number/divider == int(number/divider):
+        if number % divider == 0:
             prime = False
             break
-
     if prime:
-        #print(',', number, end='')
-        count += 1
+        #print(",", number, end='')
+        found += 1
         prime = 1
-
 end = time.monotonic()
 print(f"This took: {(end - start)} seconds.")
-print(f"I found {count} prime numbers.")
+print(f"I found {found} prime numbers.")
