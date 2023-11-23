226cd1c07a1b1cc53ff533a2b2bf045662a5cb46
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-11-23 19:23:09 +0700

    multicore improvement November 2023

---
+++ python/history/prime_4.0.py
@@ -0,0 +1,30 @@
+import math, time, multiprocessing
+
+last = 1000000
+found = 4             # we start from 11, know 2, 3, 5, 7
+queue = multiprocessing.Queue()
+
+def is_prime(number):
+    flag_prime = True
+    for divider in range(3, int(math.sqrt(number))+1, 2):
+        if number % divider == 0:
+            flag_prime = False
+            break
+    if flag_prime:
+        return number
+    else:
+        return 0
+
+if __name__ == "__main__":
+    print(f"Prime numbers to {last}")
+    start = time.perf_counter()
+    print("Create pool")
+    pool = multiprocessing.Pool()
+    results = pool.map(is_prime, range(11, last, 2))
+    pool.close()
+    for nr in results:
+        if nr > 0:
+            found += 1
+    end = time.perf_counter()
+    print(f"This took: {(end - start)} seconds.")
+    print(f"I found {found} prime numbers.")
