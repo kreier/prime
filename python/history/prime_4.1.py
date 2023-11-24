a3b6f809268c81012101e71c978ec7469e3aa591
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-11-24 17:45:57 +0700

    racecondition error

--- python/history/prime_4.1.py
+++ python/history/prime_4.1.py
@@ -4,7 +4,8 @@ import math, time, multiprocessing
 last = 10000
 found = 4             # we start from 11, know 2, 3, 5, 7
 dividers = [3, 5, 7]
-queue = multiprocessing.Queue()
+largest_divider = 7
+# queue = multiprocessing.Queue()
 
 def is_prime(number):
     flag_prime = 1
@@ -14,13 +15,21 @@ def is_prime(number):
             break
     return flag_prime
 
-def create_dividers(largest):
-    global dividers
+def create_dividers(last):
     global found
-    for number in range(11, largest, 2):
+    global largest_divider
+    div = [3, 5, 7]
+    largest_divider = int(math.sqrt(last))
+    if largest_divider % 2 == 0:
+        largest_divider += 1
+    print(f'Find dividers up to {largest_divider}.')
+    for number in range(11, largest_divider, 2):
         if is_prime(number) > 0:
             found += 1
-            dividers.append(number)
+            div.append(number)
+    div.append(largest_divider) 
+    print(div)
+    return div
 
 def is_prime2(number):
     global dividers
@@ -40,20 +49,36 @@ def is_prime2(number):
 if __name__ == "__main__":
     print(f"Prime numbers to {last}")
     start = time.perf_counter()
-    largest_divider = int(math.sqrt(last))
-    if largest_divider % 2 == 0:
-        largest_divider += 1
-    print(f'Find dividers up to {largest_divider}.')
-    create_dividers(largest_divider)
-    dividers.append(largest_divider)
-    # print(dividers)
-    print(f'Found {found} primes, now use them als dividers')
-    # Make sure the dividers are found before creating the pool!
-    time.sleep(0.5)
+    # p = multiprocessing.Process(target=create_dividers, args=(last, ))
+    # p.start()
+    # p.join()
+    lock = multiprocessing.Lock()
+    print(dividers)
+    with lock:
+        create_dividers(last)  # racecondition because of macOS?
+    print(dividers)
+
     print("Create pool")
     pool = multiprocessing.Pool()
+    # dividers = pool.apply(create_dividers, args = (last, ))
+    # result1.join()
+    # pool.apply()
+ 
+    # create_dividers(largest_divider)
+    # time.sleep(1)
+
+
+    print(dividers)
+    print(f'Found {found} primes, now use them als dividers')
+    # Make sure the dividers are found before creating the pool!
+
     results = pool.map(is_prime2, range(largest_divider + 2, last, 2))
+    # results = pool.apply(is_prime2, args = range(largest_divider + 2, last, 2))
+
     pool.close()
+    print(dividers)
+
+
     print(len(results))
     for nr in results:
         if nr > 0:
@@ -61,4 +86,5 @@ if __name__ == "__main__":
     end = time.perf_counter()
     print(f'This took: {(end - start)} seconds.')
     print(f'Found {found} primes.')
-    print(results)
+    print(dividers)
+    # print(results)
