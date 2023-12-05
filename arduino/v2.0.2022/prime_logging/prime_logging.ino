911aec8f7ab4232ca2f7e4489905284e06577a71
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-12-05 19:34:22 +0700

    extended feedback on calculation progress in Arduino

--- arduino/v2.0.2022/prime_logging/prime_logging.ino
+++ arduino/v2.0.2022/prime_logging/prime_logging.ino
@@ -1,9 +1,22 @@
-/* Prime numbers in Arduino C v2.0 2023/11/23 */
+/* Prime numbers in Arduino C v2.0 2023/12/05 */
 #include <time.h>
 #include <math.h>
 
 double start;
 int column = 10;
+
+void elapsed_time(int seconds) {
+  int hours = (int)seconds/3600;
+  int minutes = (int)(seconds/60 - hours*60);
+  int sec = (int)(seconds - minutes*60 - hours*3600);
+  Serial.print(" ");
+  Serial.print(hours);
+  Serial.print("h ");
+  Serial.print(minutes);
+  Serial.print("min ");
+  Serial.print(sec);
+  Serial.print("s ");
+}
 
 void setup() {
   Serial.begin(115200);
@@ -16,40 +29,52 @@ void loop() {
   }
   const int scope[] = {100, 1000, 10000, 100000, 1000000, 10000000, 25000000, 100000000, 1000000000};
   const int reference[] = {25, 168, 1229, 9592, 78498, 664579, 1565927, 5761455, 50847534};
-  for (int i = 0; i < 9; i++)
-  {
+  for (int i = 0; i < 9; i++) {
     int last = scope[i];
     int found = 4;   // we already know 2, 3, 5, 7
-    Serial.println("\n\nPrime v2.0 in Arduino C 2023/11/23");
+    Serial.println("\n\nPrime v2.0 in Arduino C 2023/12/05");
     Serial.print("Calculating prime numbers until ");
     Serial.println(last);
-    start = millis();      // use micros() for more precision
-    for (int number = 11; number < last; number += 2)
-    {
+    start = micros();      // use micros() for more precision
+    int dot = millis();
+    int column = 0;
+    for (int number = 11; number < last; number += 2) {
       bool prime = true;
-      for (int divider = 3; divider < (int)(sqrt(number)) + 1; divider += 2)
-      {
-        if (number % divider == 0)
-        {
+      for (int divider = 3; divider < (int)(sqrt(number)) + 1; divider += 2) {
+        if (number % divider == 0) {
           prime = false;
           break;
         }
       }
-      if (prime)
-      {
+      if (prime) {
         found += 1;
-      //  if (found % 100 == 0) { Serial.print("."); }
-      //  if (found % 10000 == 0) { Serial.println(" "); }
       }
+      if((millis() - dot) > 2000) {
+        Serial.print(".");
+        dot = millis();
+        column += 1;
+        if(column > 40) {
+          column = 0;
+          elapsed_time(dot/1000);
+          Serial.print("- ");
+          Serial.println(number);
+        }
+      }
+    }
+    float duration = (micros() - start)/1000000;
+    if(duration > 2) {
+      Serial.print("\n");
     }
     Serial.print("Found ");
     Serial.print(found);
     Serial.print(" prime numbers. It should be ");
     Serial.print(reference[i]);
     Serial.print(".\nThis took ");
-    Serial.print((millis() - start)/1000, 3);
-    Serial.println(" seconds.");
+    Serial.print(duration, 6);
+    Serial.print(" seconds.");
+    elapsed_time(duration);
   }
+  // program finished, just keep printing some dots
   for(int line = 0; line < 24; line++){
     for(int i = 0; i < 80; i++) {
       Serial.print(".");
--- arduino/v5.0.2023/prime/prime.ino
+++ arduino/v5.0.2023/prime/prime.ino
@@ -1,10 +1,10 @@
-/* Prime numbers in Arduino C v5.0 2023/12/01 */
+/* Prime numbers in Arduino C v5.0 2023/12/05 */
 /* calculate the prime numbers to the square root of the largest number, 
 use these prime numbers as dividers for the remaining numbers */
 #include <time.h>
 #include <math.h>
 
-int last = 1000000000;
+int last = 10000000;
 double start;
 int column = 10;
 int found = 4;   // we already know 2, 3, 5, 7
@@ -52,6 +52,19 @@ int is_prime_fast(int number) {
   return flag_prime;
 }
 
+void elapsed_time(int seconds) {
+  int hours = (int)seconds/3600;
+  int minutes = (int)(seconds/60 - hours*60);
+  int sec = (int)(seconds - minutes*60 - hours*3600);
+  Serial.print(" ");
+  Serial.print(hours);
+  Serial.print("h ");
+  Serial.print(minutes);
+  Serial.print("min ");
+  Serial.print(sec);
+  Serial.print("s ");
+}
+
 void setup() {
   Serial.begin(115200);
 }
@@ -61,7 +74,7 @@ void loop() {
     Serial.print(".");
     delay(1000);
   }
-  Serial.println("\n\nPrime v5.0 2023/12/01");
+  Serial.println("\n\nPrime v5.0 2023/12/05");
   Serial.print("Calculating prime numbers until ");
   Serial.println(last);
   start = millis();
@@ -75,16 +88,34 @@ void loop() {
   Serial.print(found);
   Serial.print(" primes until ");
   Serial.print(largest_divider);
-  Serial.print(" to use as dividers.\n");
+  Serial.print(" to use as divisors.\n");
+  int dot = millis();
+  int column = 0;
   for(int number = largest_divider + 2; number < last; number += 2)
   {
     found += is_prime_fast(number);
+    if((millis() - dot) > 2000) {
+      Serial.print(".");
+      dot = millis();
+      column += 1;
+      if(column > 40) {
+        column = 0;
+        elapsed_time(dot/1000);
+        Serial.print(" - ");
+        Serial.print(number);
+        Serial.print(" ");
+        Serial.print((int)number * 100 / last);
+        Serial.print("% \n");
+      }
+    }
   }
   Serial.print("\nFound ");
   Serial.print(found);
   Serial.print(" prime numbers.\nThis took ");
   Serial.print((millis() - start)/1000, 3);
-  Serial.println(" seconds.");
+  Serial.print(" seconds.");
+  elapsed_time((millis() - start)/1000);
+  Serial.print("\n");
   for(int line = 0; line < 24; line++){
     for(int i = 0; i < 80; i++) {
       Serial.print(".");
