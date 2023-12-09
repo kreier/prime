acafe3f46cee8f3477de794175faea40563a7f35
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-12-09 17:20:08 +0700

    testing 10-year old laptop

--- python/v1.1.2000/i5-2520M/prime_logging.py
+++ python/v1.1.2000/i5-2520M/prime_logging.py
@@ -1,4 +1,4 @@
-# prime v5.0.2023
+# prime v1.1.2000  
 # cycles through limits and writes to the filesystem
 
 import math, time, os, cpuinfo
@@ -7,62 +7,34 @@ scope = [100, 1000, 10000, 100000, 1000000, 10000000, 25000000, 100000000, 10000
 reference = [25, 168, 1229, 9592, 78498, 664579, 1565927, 5761455, 50847534]
 time_calc = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-def is_prime(number):
-    global found
-    flag_prime = 1
-    for divider in range(3, int(math.sqrt(number)) + 1, 2):
-        if number % divider == 0:
-            flag_prime = 0
-            break
-    return flag_prime
-
-def find_primes(largest):
-    global primes
-    global found
-    for number in range(11, largest + 1, 2):
-        if is_prime(number) > 0:
-            found += 1
-            primes.append(number)
-
-def is_prime_fast(number):
-    global found
-    flag_prime = 1
-    largest_divider = int(math.sqrt(number)) + 1
-    for divider in primes:
-        if number % divider == 0:
-            flag_prime = 0
-            break
-        if divider > largest_divider:
-            break
-    return flag_prime
-
 if __name__ == "__main__":
     for i in range(len(scope)):
         last = scope[i]
         found = 4              # we start from 11, know 2, 3, 5, 7
         primes = [3, 5, 7]     # exclude 2 since we only test odd numbers    
-        print(f"Prime numbers to {last} in Python with algorithm v5.0.2023")
+        print(f"Prime numbers to {last} in Python with algorithm v1.1.2000")
         start = time.perf_counter_ns()
-        largest_divider = int(math.sqrt(last))
-        if largest_divider % 2 == 0:
-            largest_divider += 1
-        print(f'First find prime dividers up to {largest_divider}.')
-        find_primes(largest_divider)
-        print(f'Found {found} primes, now use them als dividers.')
-        for number in range(largest_divider + 2, last, 2):
-            found += is_prime_fast(number)
+        for number in range(11, last, 2):
+            prime = True
+            for divisor in range(3, int(math.sqrt(number))+1, 2):
+                result = number / divisor
+                if int(result) == result:
+                    prime = False
+                    break
+            if prime:
+                found += 1
         end = time.perf_counter_ns()
         print(f"This took: {(end - start)/1000} microseconds.")
         print(f"I found {found} prime numbers. Should be {reference[i]}.\n")
         filename = str(last) + ".txt"
         with open(filename, "w") as fp:
-            fp.write('prime v5.0.2023 in python\n')
+            fp.write('prime v1.1.2000 in python\n')            
             fp.write(cpuinfo.get_cpu_info()['brand_raw'])
             fp.write(f'\nPrimes to {last} took {(end - start)/1000} microseconds.')
             fp.write(f'\nFound {found} primes. Should be {reference[i]}.\n')
         time_calc[i] = (end - start)/1000
     with open("summary.txt", "w")as fp:
-        fp.write('Summary of prime v5.0.2023 in python\n')
+        fp.write('Summary of prime v1.1.2000 in python\n')
         fp.write(cpuinfo.get_cpu_info()['brand_raw'])
         fp.write('\n     last       time in microseconds\n')
         for i in range(len(scope)):
