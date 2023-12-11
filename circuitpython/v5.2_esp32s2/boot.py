321136c549ecabdd522684a5a012b387c9d03e9b
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-12-11 15:13:07 +0700

    experiment from 2023/11/25

---
+++ circuitpython/v5.2_esp32s2/boot.py
@@ -0,0 +1,28 @@
+# toggle the write switch to the local file system
+
+import time
+import board
+import digitalio
+import storage
+
+button_pin = board.IO0
+# T-Display rp2040     board.BUTTON_L    False if pressed
+# T-Display ESP32-S2   board.IO0
+
+button = digitalio.DigitalInOut(button_pin)
+button.direction = digitalio.Direction.INPUT
+
+time.sleep(1)
+timer = 5
+print(f"Press the button in the next {timer} seconds to activate saving")
+end = time.monotonic() + timer
+print(timer)
+while end - time.monotonic() > 0:
+    if not button.value:
+        print("write access activated")
+        storage.remount("/", False)
+    if end - timer + 1 < time.monotonic():
+        timer -= 1
+        print(timer)
+print("Not activated")
+time.sleep(1)
